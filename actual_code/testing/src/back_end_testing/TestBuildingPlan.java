3e79bc65d8d7cab3afb1f8537112b943b6f0a87a
andreicaloian <48419225+andreicaloian@users.noreply.github.com>
2019-04-01 12:12:56 +0300

    Add files via upload

---
+++ actual_code/testing/src/back_end_testing/TestBuildingPlan.java
@@ -0,0 +1,14 @@
+package back_end_testing;
+
+import org.junit.*;
+
+public class TestBuildingPlan {
+    @Test
+    public void testParseWorkingSchedule(){}
+    @Test
+    public void testSaveWorkingSchedule(){}
+    @Test
+    public void testUpdateWorkingSchedule(){}
+    @Test
+    public void testRemoveWorkingSchedule(){}
+}
