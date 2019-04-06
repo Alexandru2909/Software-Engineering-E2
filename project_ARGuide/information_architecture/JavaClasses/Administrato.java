da6ed09b5baf8cc5ee073d7fe194e0c80c217bca
vTrStefaN <48363523+vTrStefaN@users.noreply.github.com>
2019-04-06 22:14:12 +0300

    Create Administrato.java

---
+++ project_ARGuide/information_architecture/JavaClasses/Administrato.java
@@ -0,0 +1,23 @@
+/**
+ * This class consists of two methods which represents the respective controller 
+ * which is used when an administrator want to perform an action
+ * @author stefan
+ *
+ */
+public class Administrator{
+	/**
+	 * This is the controller for building plan processes 
+	 * @param building the building plan on which actions will be performed
+	 */
+
+  public void BuildingPlanController(BuildingPlan building) {
+  }
+  /**
+   * This is the controller for working schedule processes
+   * @param schedule the working schedule on which actions will pe performed
+   */
+
+  public void ScheduleController(WorkingSchedule schedule) {
+  }
+
+}
