e839241875af18630bcb1ca15986a2dc807b8f0c
Adrian Gemeniuc <46579185+Obsession10@users.noreply.github.com>
2019-04-08 11:14:38 +0300

    Update AppMenu.java

--- ARGuideAdmin/src/main/AppMenu.java
+++ ARGuideAdmin/src/main/AppMenu.java
@@ -1,43 +1,89 @@
-/**
- * 
- */
 package main;
 
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 import javax.swing.JMenu;
 import javax.swing.JPanel;
 
 /**
- * @author Paul-Reftu
+ * @author Adrian-Gemeniuc
  *
  */
+
 public class AppMenu extends JPanel {
 
-	/* Don't forget to generate comments that are able to be 
-	 * parsed for the creation of a JavaDoc!
-	 * Automatic comment generation shortcut: Alt + Shift + J
-	*/
 	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 199316383000490255L;
-	JMenu menu;
-	/* add menu items here 
-	 * this may be of help: http://www.zentut.com/java-swing/how-to-create-menu-in-swing/ 
-	 */
 	
-	/**
-	 * 
-	 */
 	public AppMenu() {
 		init();
+		
+		// here I need the main frame of the application somehow
+		frame.setJMenuBar(createMenuBar());
 	}
 	
-	/**
-	 * 
-	 */
+	
+	
 	private void init() {
 		/* main interface implementation occurs here */
+		
+	}
+	
+	
+	/**
+	 * @return: the new menu that had been created
+	 */
+	public static JMenuBar createMenuBar() {
+
+		JMenuBar menuBar;
+		JMenu menu, submenu;
+		JMenuItem menuItem;
+
+		// BARA MENIU
+		menuBar = new JMenuBar();
+
+		// FILE Menu.
+		menu = new JMenu("File");	
+		menuBar.add(menu);
+
+		// MENIUL "NEW" -> URMAT DE UN SUBMENIU
+		menu.addSeparator();
+		submenu = new JMenu("New");
+		
+		menuItem = new JMenuItem(" Building");	
+		submenu.add(menuItem);
+		menu.add(submenu);
+
+		// OPEN-ul simplu
+		menu.addSeparator();
+		menuItem = new JMenuItem("Open");
+		menu.add(menuItem);
+
+		// SAVE-ul simplu
+		menu.addSeparator();
+		menuItem = new JMenuItem("Save");
+		menu.add(menuItem);
+
+		// SAVE AS
+		menu.addSeparator();
+		menuItem = new JMenuItem("Save as ...");
+		menu.add(menuItem);
+
+		// CLOSE
+		menu.addSeparator();
+		menuItem = new JMenuItem("Close");
+		menu.add(menuItem);
+
+		// HELP-ul din MENIU
+		menu = new JMenu("Help");
+		menuBar.add(menu);
+		
+		return menuBar;
+
 	}
 
 }
+
+
+
