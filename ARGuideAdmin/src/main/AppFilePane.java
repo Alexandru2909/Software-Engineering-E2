69582e99905f97cb248e330b5c739b9285cc0a25
Adrian Gemeniuc <46579185+Obsession10@users.noreply.github.com>
2019-04-08 11:15:41 +0300

    Update AppFilePane.java

--- ARGuideAdmin/src/main/AppFilePane.java
+++ ARGuideAdmin/src/main/AppFilePane.java
@@ -1,43 +1,70 @@
-/**
- * 
- */
+
 package main;
 
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
-/**
- * @author Paul-Reftu
- *
- */
-public class AppFilePane extends JPanel {
-	
-	/* Don't forget to generate comments that are able to be 
-	 * parsed for the creation of a JavaDoc!
-	 * Automatic comment generation shortcut: Alt + Shift + J
-	*/
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6663025488807576018L;
-	JTabbedPane filePane;
-	/* This may be of help: 
-	 * https://docs.oracle.com/javase/tutorial/uiswing/components/tabbedpane.html 
-	 */
-	
-	/**
-	 * 
-	 */
-	public AppFilePane() {
-		init();
-	}
-	
-	/**
-	 * 
-	 */
-	private void init() {
-		/* main interface implementation occurs here */
-	}
+public class AppFilePane extends JPanel {
+    
+ 
+    private static final long serialVersionUID = -6663025488807576018L;
+    
+    
+   
+    public AppFilePane() {
+        init();
+        createAndShowGUI();
+    }
+    
+    
+
+    private void init() {
+        /* main interface implementation occurs here */
+    }
+
+
+    /**
+     * starting point
+     */
+    private static void createAndShowGUI() {
+   
+        //Add content to the window.
+        frame.add(new TabbedPaneDemo(), BorderLayout.CENTER);
+      
+    }
+
+
+     /**
+     * @param text
+     * @return: displays the text from the file on every tab
+     */
+    protected JComponent makeTextPanel(String text) {
+        
+        JPanel panel = new JPanel(false);
+        JLabel filler = new JLabel(text);
+        filler.setHorizontalAlignment(JLabel.CENTER);
+        panel.setLayout(new GridLayout(1, 1));
+        panel.add(filler);
+        return panel;
+    }
+
+
+
+    public TabbedPaneDemo() {
+        
+        super(new GridLayout(1, 1));
+         
+       JTabbedPane tabbedPane = new JTabbedPane();
+       
+        addPanel(tabbedPane,"Building1.json", "panel1");   
+        addPanel(tabbedPane,"Building2.json", "panel2");   
+        addPanel(tabbedPane,"Building3.json", "panel3");  
+        addPanel(tabbedPane,"Building4.json", "panel4");
+      
+      
+        //Add the tabbed pane to this panel.
+        add(tabbedPane);            
+    }
 
 }
