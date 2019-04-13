a2f878f2f0d606a6692b940b12ed8ee73ec7711b
Adrian Gemeniuc <46579185+Obsession10@users.noreply.github.com>
2019-04-13 11:35:32 +0300

    Update AppFilePane.java

--- ARGuideAdmin/src/main/AppFilePane.java
+++ ARGuideAdmin/src/main/AppFilePane.java
@@ -1,26 +1,49 @@
-
-package main;
-
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Window;
 
+import javax.swing.BoxLayout;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 
+/**
+ * @author Adrian-Gemeniuc
+ *
+ */
+
 public class AppFilePane extends JPanel {
     
  
     private static final long serialVersionUID = -6663025488807576018L;
-    AppFrame frame;
+    public  AppFrame frame;
     
    
     public AppFilePane(AppFrame frame) {
-    	this.frame = frame;
+    	super(new GridLayout(1, 1));
+		this.frame = frame;
         init();
-        createAndShowGUI();
+       // createAndShowGUI();
+		
+       
+         
+       JTabbedPane tabbedPane = new JTabbedPane();
+       
+        addPanel(tabbedPane,"Building1.json", "panel1QKWEQWEQKWJEHQKWJEHQKWJEHKJQWHEKQJWHEKQWJEHQKWJEHQWKJEHQWJKNNSZKNDKSJNDAMNSDASNDSAKJDASJKDASKJDSAJDKHQJKWEHQKJWHEQKWJEQWUEUIFHDUSDIHAIUSDHSAIHDUIASHQIUHEIQWHEQWIUHEQIWEHQUWIHEIQWHEQWIEHQIWHEQIH656464");   
+        addPanel(tabbedPane,"Building2.json", "panel2");   
+        addPanel(tabbedPane,"Building3.json", "panel3");  
+        addPanel(tabbedPane,"Building4.json", "panel4");
+       
+        tabbedPane.setPreferredSize(new Dimension(100, 100));
+
+        
+        //Add the tabbed pane to this panel.
+        add(tabbedPane);            
     }
     
     
@@ -29,16 +52,6 @@ public class AppFilePane extends JPanel {
         /* main interface implementation occurs here */
     }
 
-
-    /**
-     * starting point
-     */
-    private static void createAndShowGUI() {
-   
-        //Add content to the window.
-        frame.add(new TabbedPaneDemo(), BorderLayout.CENTER);
-      
-    }
 
 
      /**
@@ -49,28 +62,33 @@ public class AppFilePane extends JPanel {
         
         JPanel panel = new JPanel(false);
         JLabel filler = new JLabel(text);
-        filler.setHorizontalAlignment(JLabel.CENTER);
-        panel.setLayout(new GridLayout(1, 1));
+        //Dimension d = new Dimension(30, 100);
+		//filler.setHorizontalAlignment(JLabel.CENTER);
+       
+        filler.setVerticalAlignment(JLabel.TOP);
+        filler.setPreferredSize(new Dimension(100, 100));
+        panel.setLayout(new GridLayout(1,1));
+       // panel.setSize(new Dimension(100, 100));
+        
         panel.add(filler);
         return panel;
     }
 
-
-
-    public TabbedPaneDemo() {
+    
+	/**
+	 * method to add new json files to the menu
+	 * @param tabbedPane
+	 * @param fileName
+	 * @param content -- inside the panel
+	 */
+    
+	public void addPanel(JTabbedPane tabbedPane,String fileName, String content)
+	{
+		JComponent panel = makeTextPanel(content);
+        tabbedPane.addTab(fileName, panel);
         
-        super(new GridLayout(1, 1));
-         
-       JTabbedPane tabbedPane = new JTabbedPane();
-       
-        addPanel(tabbedPane,"Building1.json", "panel1");   
-        addPanel(tabbedPane,"Building2.json", "panel2");   
-        addPanel(tabbedPane,"Building3.json", "panel3");  
-        addPanel(tabbedPane,"Building4.json", "panel4");
-      
-      
-        //Add the tabbed pane to this panel.
-        add(tabbedPane);            
-    }
+	}
+
+	
 
 }
