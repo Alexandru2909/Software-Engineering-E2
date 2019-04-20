6f3080684e5d31748a20aa92f531113389d075aa
Andrei Cristian <andreicristian6@protonmail.com>
2019-04-20 14:25:10 +0300

    each node has attributes
    Signed-off-by: Andrei Cristian <andreicristian6@protonmail.com>

--- DrawingApplication/src/Shapes/NodeShape.java
+++ DrawingApplication/src/Shapes/NodeShape.java
@@ -10,9 +10,16 @@ public class NodeShape extends Ellipse2D.Double {
 
     private Boolean isSelected;
 
+    private String name;
+    private String type;
+    private Integer floor;
+
     public NodeShape(Integer x, Integer y, Integer radius) {
         super(x - radius, y - radius, radius * 2, radius * 2);
         this.isSelected = false;
+        this.name = "";
+        this.type = "";
+        this.floor = 0;
     }
 
     public void select(int x, int y) {
@@ -30,30 +37,59 @@ public class NodeShape extends Ellipse2D.Double {
     }
 
     public void textBox(){
-        JTextField name = new JTextField();
-        JTextField type = new JTextField();
-        JTextField floor = new JTextField();
+        JTextField nameTF = new JTextField(this.getName());
+        JTextField typeTF = new JTextField(this.getType());
+        JTextField floorTF = new JTextField(this.getFloor().toString());
         JPanel panel = new JPanel(new GridLayout(0, 1));
         panel.add(new JLabel("Name:"));
-        panel.add(name);
+        panel.add(nameTF);
         panel.add(new JLabel("Type:"));
-        panel.add(type);
+        panel.add(typeTF);
         panel.add(new JLabel("Floor:"));
-        panel.add(floor);
+        panel.add(floorTF);
         int result = JOptionPane.showConfirmDialog(null, panel, "Node info", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
         if (result == JOptionPane.OK_OPTION) {
 
-            System.out.println("Name: " + name.getText());
-            System.out.println("Type: " + type.getText());
+            this.setName(nameTF.getText());
+            this.setType(typeTF.getText());
+
+            System.out.println("Name: " + nameTF.getText());
+            System.out.println("Type: " + typeTF.getText());
             try {
-                System.out.println(Integer.parseInt(floor.getText()));
+                System.out.println(Integer.parseInt(floorTF.getText()));
+                this.setFloor(Integer.parseInt(floorTF.getText()));
             } catch (Exception e) {
                 System.out.println(e);
                 System.out.println("Floor: 0");
+                this.setFloor(0);
             }
             System.out.println();
 
         }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getFloor() {
+        return floor;
+    }
+
+    public void setFloor(Integer floor) {
+        this.floor = floor;
     }
 }
