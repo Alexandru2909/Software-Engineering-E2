cc77ecc57a9df9dee2c088cfc21fdf96ee909d50
bogdanilisei <48363423+bogdanilisei@users.noreply.github.com>
2019-04-20 18:54:48 +0300

    Update Location.java

--- project_ARGuide/information_architecture/JavaPrototypes/Location.java
+++ project_ARGuide/information_architecture/JavaPrototypes/Location.java
@@ -5,7 +5,9 @@
  */
 
 public class Location {
-/**
+
+  public int id;
+  /**
  * The floor on which this location is found
  */
   public int floor;
@@ -13,11 +15,34 @@ public class Location {
    * The number of the room, if it is the case
    */
 
-  public int roomNumber;
+  public string name;
   /**
    * Returns the floor of this location
    */
-
+  
+  public int getid(){
+      return this.id;
+  };
+  
+  public int getfloor(){
+      return this.floor;
+  };
+  
+  public string getname(){
+      return this.name;
+  };
+  
+  public void setid(int idd){
+      this.id=idd;
+  };
+  
+  public void setfloor(int floorr){
+      this.floor=floorr;
+  };
+  
+  public void setname(string namee){
+      this.name=namee;
+  };
 
 
 }
