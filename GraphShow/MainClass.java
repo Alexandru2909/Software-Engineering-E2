c0a307747742f97d3b546d73c5ff074e24d0e6d7
ac999 <44598788+ac999@users.noreply.github.com>
2019-04-27 13:33:43 +0300

    Update MainClass.java

--- GraphShow/MainClass.java
+++ GraphShow/MainClass.java
@@ -417,7 +417,7 @@ public class MainClass extends JFrame {
                         if(initNode!=null){
                             curentLine.x1=initNode.xPoint+10;
                             curentLine.y1=initNode.yPoint+10;
-                            currentLine.setNode1(initNode.currentNumber);
+                            curentLine.setNode1(initNode.curentNumber);
                             drawingStage=true;
                         }
 
@@ -427,7 +427,7 @@ public class MainClass extends JFrame {
                         if(finalNode!=null){
                             curentLine.x2=finalNode.xPoint+10;
                             curentLine.y2=finalNode.yPoint+10;
-                            currentLine.setNode2(finalNode.currentNumber);
+                            curentLine.setNode2(finalNode.curentNumber);
 //                            linesList.add(new Line(curentLine.x1,curentLine.y1,curentLine.x2,curentLine.y2));
 //                            drawingSurface.repaint();
                             drawingStage=false;
