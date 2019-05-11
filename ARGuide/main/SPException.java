5439c800019332d40683c1ceeb246c4cda2c84e6
RaduMugurBogdan <48364002+RaduMugurBogdan@users.noreply.github.com>
2019-05-11 18:45:55 +0300

    Add files via upload

---
+++ ARGuide/main/SPException.java
@@ -0,0 +1,10 @@
+package IP_ThirdSprint;
+
+public class SPException extends Exception{
+    public SPException(){
+        super();
+    }
+    public SPException(String message){
+        super(message);
+    }
+}
