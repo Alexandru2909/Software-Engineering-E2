78f523dda19debaac92cb014816bdb028cd812c8
Adrian Gemeniuc <46579185+Obsession10@users.noreply.github.com>
2019-05-11 11:07:32 +0300

    Create ARGXHandler.java

---
+++ ARGuide/main/ARGXHandler.java
@@ -0,0 +1,15 @@
+package main;
+
+/*
+ * this class handles exceptions of type
+ * JSONResourseException and SPException
+ */
+ 
+public class ARGXHandler extends Exception{
+
+	ARGXHandler(String message)
+	{
+		super(message);
+	}
+  
+}
