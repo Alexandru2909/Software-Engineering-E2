f6e8ed089244ff403eb99de40ad368b406ab0c2b
RaduMugurBogdan <48364002+RaduMugurBogdan@users.noreply.github.com>
2019-05-12 21:29:48 +0300

    Add files via upload

--- ARGuide/main/SPException.java
+++ ARGuide/main/SPException.java
@@ -1,7 +1,9 @@
-package main;
+package IP_ThirdSprint;
 
-public class SPException extends ARGXHandler{
-    
+public class SPException extends Exception{
+    public SPException(){
+        super();
+    }
     public SPException(String message){
         super(message);
     }
