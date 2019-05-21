16700f6ec68cdde82fb60e1eed42436a5d504b32
unknown <dazzle.x00@gmail.com>
2019-05-21 17:03:31 +0300

    Ask for permissions on application startup

--- Project/app/src/main/java/com/frontend/frontend/Main/MainActivity.java
+++ Project/app/src/main/java/com/frontend/frontend/Main/MainActivity.java
@@ -1,12 +1,17 @@
 package com.frontend.frontend.Main;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.frontend.frontend.ImageProcessing;
 import com.frontend.frontend.R;
@@ -21,10 +26,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[] {
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                        Manifest.permission.CAMERA
+                },
+                100);
+
 
         startOcrBtn = findViewById(R.id.startOcrBtn);
         startOcrBtn.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +65,20 @@ public class MainActivity extends AppCompatActivity {
             }
             if (resultCode == Activity.RESULT_CANCELED) {
                 // Do something if no result got
+            }
+        }
+    }
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 100){
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[2] == PackageManager.PERMISSION_GRANTED) {
+            } else {
+                Toast.makeText(this, "Permission is denied.", Toast.LENGTH_SHORT).show();
+                finish();
             }
         }
     }
